5cf33ca879b3d05c2efc439a49270649d1322cc1
adel <adel.fantasy11@gmail.com>
2024-05-03 01:02:34 +0300

    New Changes

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -33,27 +33,9 @@ class App extends AppHelpers {
     this.status = 'ready';
     document.dispatchEvent(new CustomEvent('theme::ready'));
     this.log('Theme Loaded 🎉');
-    this.Swiper();
   }
 
-  Swiper(){
-    var swiper = new Swiper(".mySwiper", {
-        effect: "coverflow",
-        grabCursor: true,
-        centeredSlides: true,
-        slidesPerView: "auto",
-        coverflowEffect: {
-            rotate: 50,
-            stretch: 0,
-            depth: 100,
-            modifier: 1,
-            slideShadows: true,
-        },
-        pagination: {
-            el: ".swiper-pagination",
-        },
-    });
-  }
+
 
   log(message) {
     salla.log(`ThemeApp(Raed)::${message}`);
